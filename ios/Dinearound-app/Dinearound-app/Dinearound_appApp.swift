e72c7f20871f7b680e108135137ea100441a7874
sami salehin <coding@samis-Mac-mini.local>
2025-09-14 17:17:26 -0700

    init(ios): add SwiftUI Xcode project under ios/

---
+++ ios/Dinearound-app/Dinearound-app/Dinearound_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dinearound_appApp.swift
+//  Dinearound-app
+//
+//  Created by sami salehin on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Dinearound_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Dinearound-app/Dinearound-appTests/Dinearound_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dinearound_appTests.swift
+//  Dinearound-appTests
+//
+//  Created by sami salehin on 9/14/25.
+//
+
+import Testing
+@testable import Dinearound_app
+
+struct Dinearound_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
